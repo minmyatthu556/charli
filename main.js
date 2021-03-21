71b38e49e1f08e2e6834a1c50f6c8170e9801cfd
minmyatthu <mmthu17mce@ytu.edu.mm>
2021-03-21 21:37:14 +0630

    first commit

---
+++ main.js
@@ -0,0 +1,83 @@
+let a1click = document.getElementById('a1-click');
+let a1tracks = document.getElementById('a1-tracklist');
+a1tracks.style.display = "none";
+
+a1click.onclick = function () {
+    if (a1tracks.style.display === "none") {
+        a1tracks.style.display = "block";
+    } else {
+        a1tracks.style.display = "none";
+    }
+}
+
+let a2click = document.getElementById('a2-click');
+let a2tracks = document.getElementById('a2-tracklist');
+a2tracks.style.display = "none";
+
+a2click.onclick = function () {
+    if (a2tracks.style.display === "none") {
+        a2tracks.style.display = "block";
+    } else {
+        a2tracks.style.display = "none";
+    }
+}
+
+let a3click = document.getElementById('a3-click');
+let a3tracks = document.getElementById('a3-tracklist');
+a3tracks.style.display = "none";
+
+a3click.onclick = function () {
+    if (a3tracks.style.display === "none") {
+        a3tracks.style.display = "block";
+    } else {
+        a3tracks.style.display = "none";
+    }
+}
+
+let a4click = document.getElementById('a4-click');
+let a4tracks = document.getElementById('a4-tracklist');
+a4tracks.style.display = "none";
+
+a4click.onclick = function () {
+    if (a4tracks.style.display === "none") {
+        a4tracks.style.display = "block";
+    } else {
+        a4tracks.style.display = "none";
+    }
+}
+
+let a5click = document.getElementById('a5-click');
+let a5tracks = document.getElementById('a5-tracklist');
+a5tracks.style.display = "none";
+
+a5click.onclick = function () {
+    if (a5tracks.style.display === "none") {
+        a5tracks.style.display = "block";
+    } else {
+        a5tracks.style.display = "none";
+    }
+}
+
+let a6click = document.getElementById('a6-click');
+let a6tracks = document.getElementById('a6-tracklist');
+a6tracks.style.display = "none";
+
+a6click.onclick = function () {
+    if (a6tracks.style.display === "none") {
+        a6tracks.style.display = "block";
+    } else {
+        a6tracks.style.display = "none";
+    }
+}
+
+let a7click = document.getElementById('a7-click');
+let a7tracks = document.getElementById('a7-tracklist');
+a7tracks.style.display = "none";
+
+a7click.onclick = function () {
+    if (a7tracks.style.display === "none") {
+        a7tracks.style.display = "block";
+    } else {
+        a7tracks.style.display = "none";
+    }
+}
